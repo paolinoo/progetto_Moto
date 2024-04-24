38473e5b068b163a01ceac9422b43271af8282d3
paolinoo <paologuerini32@gmail.com>
2024-04-24 07:59:58 +0200

    inizio progetto

---
+++ src/main/java/com/mycompany/progetto_moto/Progetto_Moto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.progetto_moto;
+
+/**
+ *
+ * @author Studente
+ */
+public class Progetto_Moto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
